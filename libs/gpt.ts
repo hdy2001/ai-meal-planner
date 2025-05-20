28ee5b949f6c6ddee4dfc6ca855c417af7b5e30a
hdy2001 <2322766243@qq.com>
2025-05-20 19:09:29 +0800

    首次创建网站内容

---
+++ libs/gpt.ts
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+// Use this if you want to make a call to OpenAI GPT-4 for instance. userId is used to identify the user on openAI side.
+export const sendOpenAi = async (
+  messages: any[], // TODO: type this
+  userId: string,
+  max = 1000,
+  temp = 1
+) => {
+  const url = 'https://api.vveai.com/v1/chat/completions';
+
+  console.log('Ask GPT >>>');
+  messages.map((m) =>
+    console.log(' - ' + m.role.toUpperCase() + ': ' + m.content)
+  );
+
+  const body = JSON.stringify({
+    model: 'gpt-4',
+    messages,
+    max_tokens: max,
+    temperature: temp,
+    user: userId,
+  });
+
+  const options = {
+    headers: {
+      Authorization: `Bearer sk-ustJ6AJdEvbeeajS7e4dEf17CcE3419c901cAdE81c2e7cC8`,
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const res = await axios.post(url, body, options);
+
+    const answer = res.data.choices[0].message.content;
+    const usage = res?.data?.usage;
+
+    console.log('>>> ' + answer);
+    console.log(
+      'TOKENS USED: ' +
+      usage?.total_tokens +
+      ' (prompt: ' +
+      usage?.prompt_tokens +
+      ' / response: ' +
+      usage?.completion_tokens +
+      ')'
+    );
+    console.log('\n');
+
+    return answer;
+  } catch (e) {
+    console.error('GPT Error: ' + e?.response?.status, e?.response?.data);
+    return null;
+  }
+};
